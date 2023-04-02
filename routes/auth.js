f4a5359af93a58439cb77d3899cb6f04748ca6a0
Majid Aziz <majidaziz7ctech@gmail.com>
2023-04-02 05:30:35 +0500

    fixes auth

--- routes/auth.js
+++ routes/auth.js
@@ -5,36 +5,50 @@ const bcrypt = require('bcrypt')
 
 //REGISTER
 router.post('/register', async (req, res) => {
-  try {
-      const { username, email, password } = req.body;
-      const hashed = await bcrypt.hash(password, 12)
-      const newUser = new User({
-          username,
-          email,
-          password: hashed
-      })
-      const user = await newUser.save()
-      res.status(201).json(user)
-  } catch (error) {
-      res.status(401).json(error.mesage)
-  }
+    try {
+        const userExists = await User.findOne({email : req.body.email})
+        if(userExists) {
+            res.status(400).json("user already exists")
+        }
+        const { username, email, password } = req.body;
+        const hashed = await bcrypt.hash(password, 12)
+        const newUser = new User({
+            username,
+            email,
+            password: hashed
+        })
+        const user = await newUser.save()
+        res.status(201).json(user)
+    } catch (error) {
+        res.status(401).json(error.mesage)
+    }
 })
 
 // LOGIN
 
 router.post('/login', async (req, res) => {
-  try {
-      const userExists = await User.findOne({ email: req.body.email })
-      !userExists && res.status(400).json("User Not Exists")
-      const pass = await bcrypt.compare(req.body.password, userExists.password)
-      if (!pass) {
-          res.status(401).json("Incorrect email or Password")
-      }
-      const { password, ...others } = userExists._doc;
-      res.status(201).json(others)
-  } catch (error) {
-      res.status(401).json(error.mesage)
-  }
+    try {
+        const userExists = await User.findOne({ email: req.body.email })
+        !userExists && res.status(400).json("User Not Exists")
+        const pass = await bcrypt.compare(req.body.password, userExists.password)
+        if (!pass) {
+            res.status(401).json("Incorrect email or Password")
+        }
+
+        const accessToken = jwt.sign(
+            {
+                id: pass._id,
+                isAdmin: pass.isAdmin,
+            },
+            "secretmajid",
+            { expiresIn: "3d" }
+        );
+
+        const { password, ...others } = userExists._doc;
+        res.status(201).json({ others, accessToken })
+    } catch (error) {
+        res.status(401).json(error.mesage)
+    }
 })
 
 
